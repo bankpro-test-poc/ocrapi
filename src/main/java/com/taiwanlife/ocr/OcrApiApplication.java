73493797b670bfdb3385c19a15b9ea46058c1a0c
henry_hsieh <henry_hsieh@bankpro.com.tw>
2022-02-24 22:10:52 +0800

    初始匯入

---
+++ src/main/java/com/taiwanlife/ocr/OcrApiApplication.java
@@ -0,0 +1,23 @@
+package com.taiwanlife.ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OcrApiApplication {
+
+    @GetMapping("/api/ocr")
+    public String ocr() {
+        return "OCR API had been called!!!";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OcrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taiwanlife/ocr/OcrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiwanlife.ocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
